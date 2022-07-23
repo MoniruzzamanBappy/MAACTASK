988d4908fb4de877fe43ad463fd447b7f8552a9a
MoniruzzamanBappy <mdmoniruzzamanbappy@gmail.com>
2022-07-23 23:37:33 +0600

    added the login and sign api and design

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -3,14 +3,24 @@ import Header from "../Header/Header";
 import Footer from "./../Footer/Footer";
 import background from "../../Media/bgdot.png";
 
-const Login = () => {
-  const handleLogin = async (e) => {
+const Signup = () => {
+  const handleSignup = async (e) => {
     e.preventDefault();
     const employeeId = e.target.employeeId.value;
     const password = e.target.password.value;
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const phoneNumber = e.target.number.value;
+    const role = e.target.role.value;
+    const passwordConfirm = e.target.confirmPassword.value;
     var raw = {
+      name,
+      email,
       employeeId,
       password,
+      passwordConfirm,
+      role,
+      phoneNumber,
     };
 
     var requestOptions = {
@@ -19,7 +29,7 @@ const Login = () => {
       redirect: "follow",
     };
 
-    fetch("https://staging-api.erpxbd.com/api/v1/users/login", requestOptions)
+    fetch("https://staging-api.erpxbd.com/api/v1/users/signup", requestOptions)
       .then((response) => response.text())
       .then((result) => console.log(result))
       .catch((error) => console.log("error", error));
@@ -30,7 +40,7 @@ const Login = () => {
         <Header />
         <div class="hero">
           <div class="hero-content flex-col lg:flex-row">
-            <section class="h-screen">
+            <section>
               <div class="container px-6 py-6 h-full">
                 <div class="flex justify-center items-center flex-wrap  g-6 text-gray-800">
                   <div class="md:w-8/12 lg:w-6/12 mb-12 md:mb-0">
@@ -42,9 +52,30 @@ const Login = () => {
                   </div>
                   <div class="md:w-8/12 lg:w-5/12 lg:ml-20">
                     <h1 className="text-center font-bold mb-3 text-3xl">
-                      Please Login
+                      Create Account
                     </h1>
-                    <form onSubmit={handleLogin}>
+                    <p className="text-center my-4">
+                      Fill in the details below to create an account
+                    </p>
+                    <form onSubmit={handleSignup}>
+                      <div class="mb-6">
+                        <input
+                          type="text"
+                          name="name"
+                          class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                          placeholder="Your Full Name"
+                        />
+                      </div>
+
+                      <div class="mb-6">
+                        <input
+                          type="email"
+                          name="email"
+                          class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                          placeholder="Your Email"
+                        />
+                      </div>
+
                       <div class="mb-6">
                         <input
                           type="text"
@@ -56,27 +87,55 @@ const Login = () => {
 
                       <div class="mb-6">
                         <input
+                          type="number"
+                          name="number"
+                          class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                          placeholder="Phone Number"
+                        />
+                      </div>
+
+                      <div class="mb-6">
+                        <input
                           type="password"
                           name="password"
                           class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                           placeholder="Password"
                         />
                       </div>
+
+                      <div class="mb-6">
+                        <input
+                          type="password"
+                          name="confirmPassword"
+                          class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                          placeholder="Confirm Password"
+                        />
+                      </div>
+
+                      <div class="mb-6">
+                        <select
+                          class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                          name="role"
+                        >
+                          <option value="HUB">HUB</option>
+                        </select>
+                      </div>
+
                       <button
                         type="submit"
                         class="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full"
                         data-mdb-ripple="true"
                         data-mdb-ripple-color="light"
                       >
-                        Sign in
+                        Create Account
                       </button>
                     </form>
                     <div class="flex items-center my-4 before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5">
                       <p class="text-center font-semibold mx-4 mb-0">OR</p>
                     </div>
                     <h1 className="text-xl text-center">
-                      Don’t have any account?{" "}
-                      <span className="text-primary">Sign Up</span>
+                      Already have an account?{" "}
+                      <span className="text-primary">Sign in</span>
                     </h1>
                   </div>
                 </div>
@@ -90,4 +149,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default Signup;
